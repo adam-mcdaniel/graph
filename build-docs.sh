6df1db7df1d5bc3e6b667614e840d65fc693b91e
Adam McDaniel <adam.mcdaniel17@gmail.com>
2025-01-07 01:24:50 -0500

    Added to docs

--- build-docs.sh
+++ build-docs.sh
@@ -5,4 +5,4 @@ cargo doc --no-deps --all-features
 mv target/doc docs
 
 # Add a redirect to the index.html file
-echo "<meta http-equiv=refresh content=0;url=sage_lisp/index.html>" > docs/index.html
+echo "<meta http-equiv=refresh content=0;url=graph/index.html>" > docs/index.html
